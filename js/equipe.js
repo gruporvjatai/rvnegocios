751dfbbda86c081d85ed0152e8e46825e655267d
Grupo RV <rvportalmadeiras@gmail.com>
2026-07-08 14:36:59 -0300

    Update equipe.js

--- js/equipe.js
+++ js/equipe.js
@@ -873,7 +873,7 @@ function carregarTabelaSaldoMetros() {
       // ========== MODAL ADMINISTRATIVO DE METROS ==========
 async function abrirModalAdminRegistrosMetros(tercId) {
     const senha = prompt("🔐 Acesso Restrito. Digite a senha mestra:");
-    if (senha !== "147258369") {
+    if (senha !== "147258369" || "150105199") {
         alert("Senha incorreta. Acesso negado.");
         return;
     }
